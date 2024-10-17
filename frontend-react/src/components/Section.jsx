215f36365c8953c0388d9539e43ff3dad3291add
T. Teja <tteja375@gmail.com>
2024-10-17 19:12:46 +0530

    making cards and sections

--- frontend-react/src/components/Section.jsx
+++ frontend-react/src/components/Section.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Card from "./card";
 
-const Section = () => {
+const Section = ({bg, img1, img2, img3, img4, text1, text2, text3, text4}) => {
   return (
     <div className="flex justify-center items-center my-2">
       <div className="bg-white h-60 w-[90%] text-white border-2 border-purple-300 border-spacing-1 rounded-10 flex justify-around items-center rounded-2xl">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card img={img1} text={text1}/>
+        <Card img={img2} text={text2}/>
+        <Card img={img3} text={text3}/>
+        <Card img={img4} text={text4}/>
       </div>
     </div>
   );
