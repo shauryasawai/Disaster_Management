648191df1e93a58c03f4b8b4fd7727b480de515b
Shauryaman Sawai <sawaisushil@gmail.com>
2024-10-20 13:05:02 +0530

    effort dep

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
 
 echo "BUILD START"
-
-# Install dependencies in a local directory
 python3.9 -m pip install -r requirements.txt --target ./package
-
-# Navigate into the package directory and zip dependencies
 cd package
 zip -r9 ../function.zip .
-
-# Navigate back to the root project directory
 cd ..
-
-# Add your Django project files to the zip
 zip -r9 function.zip .
-
-
 echo "BUILD END"
