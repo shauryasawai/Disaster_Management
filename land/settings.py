df309874280f5a579c61afeef143777b39a7027d
Shauryaman Sawai <sawaisushil@gmail.com>
2024-10-20 12:23:44 +0530

    no sta

--- land/settings.py
+++ land/settings.py
@@ -149,8 +149,7 @@ DATE_FORMAT = "d/m/y"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_URL = 'static/'
 
 
 # Default primary key field type
