66e0e135dda78d591dffbea834e1aeff94cea10c
Shauryaman Sawai <sawaisushil@gmail.com>
2024-10-20 15:25:52 +0530

    host fix

--- land/settings.py
+++ land/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1","localhost:3000","localhost"]
 
 
 # Application definition
